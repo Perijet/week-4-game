30741fbc3bbb8c3dca40ef6dc7bcc581c921c724
Nigel John <nigeljohn@Nigels-MacBook-Pro.local>
2016-04-27 23:45:29 -0400

    Updated .js file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,9 @@
 var lukeSkywalker = {
 	name: "Luke Skywalker",
 	health: 100,
-	attack: 5,
-	constant: 5,
-	counter: 10,
+	attack: 10,
+	constant: 10,
+	counter: 5,
 };
 
 var obiWan = {
@@ -11,23 +11,23 @@ var obiWan = {
 	health: 120,
 	attack: 8,
 	constant: 8,
-	counter: 0,
+	counter: 10,
 };
 
 var darthSidious = {
 	name: "Darth Sidious",
 	health: 150,
-	attack: 20,
-	constant: 20,
-	counter: 10,
+	attack: 15,
+	constant: 15,
+	counter: 20,
 };
 
 var darthMaul = {
 	name: "Darth Maul",
 	health: 180,
-	attack: 25,
-	constant: 125,
-	counter: 10,
+	attack: 20,
+	constant: 20,
+	counter: 25,
 };
 
 $(document).ready(function(){	
@@ -41,7 +41,6 @@ $('.enemySection').click(function(){
 });
 
 $('.fight').click(attack);
-//$('#startOver').click(reset);
 
 });
 
@@ -108,17 +107,26 @@ opponent.health -= fighter.attack;
 fighter.health -= opponent.attack;
 fighter.attack += fighter.constant;
 
-$('#caption1').html('You attacked ' + opponent.name + ' for ' + fighter.attack + ' damages');
-$('#caption2').html(opponent.name + ' attacked you back for ' + opponent.attack + ' damages');
 
-if(fighter.health > 0 && opponent.health <= 0){
-	console.log('You win');
+
+$('#caption1').html('You attacked ' + opponent.name + ' for ' + fighter.attack + ' damages' + 
+	'<br>' + opponent.name + ' attacked you back for ' + opponent.attack + ' damages');
+//$('#caption2').html(opponent.name + ' attacked you back for ' + opponent.attack + ' damages');
+
+if(fighter.health >= 0 && opponent.health <= 0){
+	$('#caption2').html('You have defeated ' + opponent.name + ' you can choose to fight another enemy');
 	$('.defense div').empty();
+	$('#caption1').empty();
+	
+	console.log('You win');
 }
-else if(opponent.health > 0 && fighter.health <= 0){
+else if(opponent.health >= 0 && fighter.health <= 0){
+	$('#caption2').html('You have been defeated game over');
+	$('#caption1').empty();
 	console.log('The force wins');
-
 }
+
+
 else if($.isEmptyObject('.enemySection')){
 	$('#caption1').html('You win game over!!!');
 	$('#billboard').append('<button id="startOver">Reset</button>');
@@ -127,13 +135,14 @@ else if($.isEmptyObject('.enemySection')){
 	
 }
 
-//$('score1').html(opponent.health);
-
 
 
 }
 
 function reset(){
 	location.reload();
+}
+function disable(){
+	$('#caption2').html('You have been defeated game over');
 }
 
